659ee25296f0c2bba89c24f98a3225408c3154ff
Anitha Deenadayalan <AnithaAmalAnand@gmail.com>
2023-06-04 17:03:52 +0800

    Create LinearSearch.py
    JC1

---
+++ LinearSearch.py
@@ -0,0 +1,7 @@
+list = [96,74,55,30,65,41,89]
+numToFind=65
+for index in range(len(list)):
+    # print (str(index) + "-" + str(list[index])) 
+    if list[index] == 65:
+        print(index)
+        break
